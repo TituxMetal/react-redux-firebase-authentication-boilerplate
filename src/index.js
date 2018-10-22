5c95c845353c7367849e5c8b45d9b52163a72817
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-22 10:37:31 +0200

    Update index.js file; add materialize css and js, add support for responsive navbar and cleanup semi-colons and comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'materialize-css/dist/css/materialize.min.css'
+import Materialize from 'materialize-css/dist/js/materialize'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidenav = document.querySelectorAll('.sidenav')
+  Materialize.Sidenav.init(sidenav)
+})
+
+serviceWorker.unregister()
