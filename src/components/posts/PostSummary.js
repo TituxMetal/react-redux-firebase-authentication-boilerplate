44bf0e6040766319c7c162c3fe237692b47fbb17
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-23 08:28:04 +0200

    Add PostSummary component

---
+++ src/components/posts/PostSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <article className="card z-depth-0 post-summary">
+      <div className="card-content purple grey-text text-lighten-4">
+        <span className="card-title">Post Title</span>
+        <p>Posted by Tuxi Metal</p>
+        <p className="grey-text text-lighten-1">2018 18th August, 13h37</p>
+      </div>
+    </article>
+  )
+}
