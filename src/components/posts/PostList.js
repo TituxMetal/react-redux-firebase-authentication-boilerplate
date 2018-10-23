e995df56592c0a02074fd5fa4ccb33d9a54c4043
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-23 08:29:12 +0200

    Add PostList component

---
+++ src/components/posts/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostSummary from './PostSummary';
+
+export default () => {
+  return (
+    <section className="post-list section">
+      <PostSummary />
+      <PostSummary />
+      <PostSummary />
+      <PostSummary />
+    </section>
+  )
+}
