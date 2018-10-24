a1444590330e2e7076d65f48c95ebc37864914fa
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-24 10:23:01 +0200

    Add LoginForm component

---
+++ src/components/auth/LoginForm.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import './AuthForm.css'
+import AuthButton from './AuthButton'
+
+export default ({ changeHandler, submitHandler }) => {
+  return (
+    <form className='row' onSubmit={submitHandler}>
+      <div className='col s12 m8 offset-m2'>
+        <div className='input-field'>
+          <label htmlFor='email'>Email</label>
+          <input
+            onChange={changeHandler}
+            className='grey-text text-lighten-2'
+            type='email'
+            id='email'
+            name='email'
+          />
+        </div>
+        <div className='input-field'>
+          <label htmlFor='password'>Password</label>
+          <input
+            onChange={changeHandler}
+            className='grey-text text-lighten-2'
+            type='password'
+            id='password'
+            name='password'
+          />
+        </div>
+        <AuthButton text='Login' />
+      </div>
+    </form>
+  )
+}
