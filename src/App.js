892e244f66b5922f2a981067a1e8518d8a1ae226
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-24 10:31:58 +0200

    Update App.js file; add routes for login and register components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './containers/Dashboard';
 import PostDetails from './containers/PostDetails';
+import Login from './containers/Login';
+import Register from './containers/Register';
 
 export default () => (
   <BrowserRouter>
@@ -12,6 +14,8 @@ export default () => (
       <Switch>
         <Route path='/' exact component={Dashboard} />
         <Route path='/post/:id' component={PostDetails} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
       </Switch>
     </Fragment>
   </BrowserRouter>
