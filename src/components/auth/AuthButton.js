6db2d61a7d78acee1fc0ab7e231f5c7a87d7bcf0
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-24 10:21:39 +0200

    Add AuthButton component

---
+++ src/components/auth/AuthButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default ({ text }) => {
+  return (
+    <div className="input-field center">
+      <button className="btn-large purple lighten-1 z-depth-0">{text}</button>
+    </div>
+  )
+}
