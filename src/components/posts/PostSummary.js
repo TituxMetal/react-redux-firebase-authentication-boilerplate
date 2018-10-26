8a2721fc8c6daf210f2affbd7cc469fb08b17cb5
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-26 10:50:56 +0200

    Update PostSummary component; add post props

--- src/components/posts/PostSummary.js
+++ src/components/posts/PostSummary.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default () => {
+export default ({ post }) => {
   return (
     <article className="card z-depth-0 post-summary">
       <div className="card-content purple grey-text text-lighten-4">
-        <span className="card-title">Post Title</span>
+        <span className="card-title">
+          { post.title }
+        </span>
         <p>Posted by Tuxi Metal</p>
         <p className="grey-text text-lighten-1">2018 18th August, 13h37</p>
       </div>
