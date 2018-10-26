616a2eeb7e254a5f9b30af6a627c312eb4f467ba
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-26 10:48:02 +0200

    Update index.js file; add redux provider and redux store to the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import 'materialize-css/dist/css/materialize.min.css'
 import Materialize from 'materialize-css/dist/js/materialize'
-import './index.css'
 
+import './index.css'
 import App from './App'
+import rootReducer from './store/reducers'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const app = <Provider store={store}><App /></Provider>
+
+ReactDOM.render(app, document.getElementById('root'))
 
 document.addEventListener('DOMContentLoaded', () => {
   const sidenav = document.querySelectorAll('.sidenav')
