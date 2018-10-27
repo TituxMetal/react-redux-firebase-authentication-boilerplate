a9b4f2561087a180be19a581a4c63dbef31db032
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-28 01:16:52 +0200

    Update post action file; update createPost action to add post in firestore database

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -2,6 +2,14 @@ import * as actionTypes from './actionTypes'
 
 export const createPost = post => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
-    dispatch({ type: actionTypes.ADD_POST, post })
+    const firestore = getFirestore()
+    firestore.collection('posts').add({
+      ...post,
+      authorFirstName: 'Tuxi',
+      authorLastName: 'Metal',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => dispatch({ type: actionTypes.ADD_POST, post })
+    ).catch(error => dispatch({ type: actionTypes.ADD_POST_ERROR, error }))
   }
 }
