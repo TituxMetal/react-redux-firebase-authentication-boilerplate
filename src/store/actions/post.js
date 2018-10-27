9a8f75319093b46dca2f61eb2caecafee44e17a7
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-27 23:02:09 +0200

    Update post action; add firebase and firestore access

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,7 +1,7 @@
 import * as actionTypes from './actionTypes'
 
 export const createPost = post => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     dispatch({ type: actionTypes.ADD_POST, post })
   }
 }
