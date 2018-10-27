36f889a5e0ee4f6f5a9601ed51e8fc6b4b62e3a5
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-27 23:03:40 +0200

    Update index.js file; add support for firestore and firebase

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import 'materialize-css/dist/css/materialize.min.css'
 import Materialize from 'materialize-css/dist/js/materialize'
+import { reduxFirestore, getFirestore }from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 
 import './index.css'
 import App from './App'
+import firebaseConfig from './config/firebase'
 import rootReducer from './store/reducers'
 import * as serviceWorker from './serviceWorker'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    compose(
+      applyMiddleware(thunk.withExtraArgument({
+        getFirebase,
+        getFirestore
+      })),
+      reduxFirestore(firebaseConfig),
+      reactReduxFirebase(firebaseConfig)
+    )
+  )
+)
 const app = <Provider store={store}><App /></Provider>
 
 ReactDOM.render(app, document.getElementById('root'))
