b8fef439e65b66d7363534e1c13eac972a41af9b
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-28 07:23:13 +0100

    Update PostDetails container component; connect component with redux store and firebase and add a spinner component

--- src/containers/PostDetails.js
+++ src/containers/PostDetails.js
@@ -1,18 +1,40 @@
 import React from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import SpinnerWrapper from '../components/spinner/SpinnerWrapper'
 
-export default ({ match: { params } }) => {
-  const postId = params.id
+const PostDetails = ({ post }) => {
+  const details = post ? (
+    <article className="card-panel purple grey-text text-lighten-4">
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      <hr />
+      <p>Posted by {`${post.authorFirstName} ${post.authorLastName}`}</p>
+      <p className="grey-text text-lighten-1">2018 18th August, 13h37</p>
+    </article>
+  ) : (
+    <SpinnerWrapper />
+  )
   return (
     <section className="container section post-details">
-      <article className="card-panel purple grey-text text-lighten-4">
-        <h2>Post Title - {postId}</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repellat accusantium officia aperiam dolorem fuga, ad blanditiis illum ullam quibusdam deleniti iure minima expedita voluptate a mollitia dignissimos sed laboriosam.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repellat accusantium officia aperiam dolorem fuga, ad blanditiis illum ullam quibusdam deleniti iure minima expedita voluptate a mollitia dignissimos sed laboriosam.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repellat accusantium officia aperiam dolorem fuga, ad blanditiis illum ullam quibusdam deleniti iure minima expedita voluptate a mollitia dignissimos sed laboriosam.</p>
-        <hr/>
-        <p>Posted by Tuxi Metal</p>
-        <p className="grey-text text-lighten-1">2018 18th August, 13h37</p>
-      </article>
+      {details}
     </section>
   )
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id
+  const posts = state.firestore.data.posts
+  const post = posts ? posts[id] : null
+  return {
+    post
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'posts' }
+  ])
+)(PostDetails)
